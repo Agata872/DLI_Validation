b96d0e4a806e00a76b58cb96f0b98fb70ae2f9d5
Tianzheng <73996507+Agata872@users.noreply.github.com>
2025-06-17 15:10:22 +0200

    Update combingTxRx.py

--- Measure/combingTxRx.py
+++ Measure/combingTxRx.py
@@ -49,7 +49,8 @@ def run_remote_script(target, script_path):
 
 def main():
     TX_NAME = "T01"
-    RX_NAMES = ["M01"]  # ceiling
+    #RX_NAMES = ["M01"]  # ceiling
+    RX_NAMES = ceiling
 
     inventory_file = "inventory.yaml"
     inventory = load_inventory(inventory_file)
