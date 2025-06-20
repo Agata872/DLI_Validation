3e1627a2d70c263f85ea7259133e2fc7889212de
Agata872 <2310123801@qq.com>
2025-06-20 08:02:05 +0200

    change

--- Measure/downlink.py
+++ Measure/downlink.py
@@ -62,7 +62,8 @@ def main():
     user = inventory.get("all", {}).get("vars", {}).get("ansible_user", "pi")
     all_hosts = inventory.get("all", {}).get("hosts", {})
 
-    SCRIPT_REMOTE_PATH = "~/DLI_Validation/Measure/RxCSI_TX_DL.py"
+    # SCRIPT_REMOTE_PATH = "~/DLI_Validation/Measure/RxCSI_TX_DL.py"
+    SCRIPT_REMOTE_PATH = "~/DLI_Validation/Measure/beamform.py"
 
     threads = []
     for host in hosts:
