4f9436f63acb91e729c7e479ebf158fc4f8a81ef
Gilles Callebaut <callebaut.gilles@gmail.com>
2025-06-24 14:19:49 +0200

    Update beamform.py

--- Measure/double-pilot/beamform.py
+++ Measure/double-pilot/beamform.py
@@ -799,9 +799,9 @@ def main():
             except yaml.YAMLError as exc:
                 print(exc)
 
-        PHI_CSI = PHI_PR_1 - PHI_CABLE #+ PHI_CABLE
+        PHI_CSI = PHI_PR_1 + PHI_CABLE #+ PHI_CABLE
 
-        PHI_MRT = -(PHI_CSI + PHI_LR - PHI_CABLE)  # + PHI_CABLE
+        PHI_MRT = -(PHI_CSI + PHI_LR + PHI_CABLE)  # + PHI_CABLE
 
         # PHI_DLI =
         # benchmark without phased beamforming
