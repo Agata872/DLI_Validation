b40a02d4d1ca8b549427ee32f639e259460a730e
Gilles Callebaut <callebaut.gilles@gmail.com>
2025-07-01 10:28:20 +0200

    Update record-measurements.py

--- Measure/double-pilot/record-measurements.py
+++ Measure/double-pilot/record-measurements.py
@@ -24,43 +24,43 @@ positioner = PositionerClient(config=config["positioning"], backend="direct")
 positioner.start()
 
 
-# script_started = time()
+script_started = time()
 
-# context = zmq.Context()
+context = zmq.Context()
 
-# iq_socket = context.socket(zmq.PUB)
+iq_socket = context.socket(zmq.PUB)
 
-# iq_socket.bind(f"tcp://*:{50002}")
+iq_socket.bind(f"tcp://*:{50002}")
 
 
-# def wait_till_go_from_server(ip="192.108.1.147"):
+def wait_till_go_from_server(ip="192.108.1.147"):
 
-#     global meas_id, file_open, data_file, file_name
-#     context = zmq.Context()
-#     alive_socket = context.socket(zmq.REQ)
-#     alive_socket.connect("tcp://192.108.1.147:5560")  # <== IP 是服务器那台的
+    global meas_id, file_open, data_file, file_name
+    context = zmq.Context()
+    alive_socket = context.socket(zmq.REQ)
+    alive_socket.connect("tcp://192.108.1.147:5560")  # <== IP 是服务器那台的
 
-#     # Sending alive message
-#     alive_socket.send_string("client_ready")
-#     response = alive_socket.recv_string()
-#     print("Server response:", response)
-#     # Connect to the publisher's address
-#     print("Connecting to server %s.", ip)
-#     sync_socket = context.socket(zmq.SUB)
-#     sync_socket.connect(f"tcp://{ip}:{5559}")
-#     # Subscribe to topics
-#     sync_socket.subscribe("")
+    # Sending alive message
+    alive_socket.send_string("client_ready")
+    response = alive_socket.recv_string()
+    print("Server response:", response)
+    # Connect to the publisher's address
+    print("Connecting to server %s.", ip)
+    sync_socket = context.socket(zmq.SUB)
+    sync_socket.connect(f"tcp://{ip}:{5559}")
+    # Subscribe to topics
+    sync_socket.subscribe("")
 
-#     # Receives a string format message
-#     print("Waiting on SYNC from server %s.", ip)
+    # Receives a string format message
+    print("Waiting on SYNC from server %s.", ip)
 
-#     meas_id, unique_id = sync_socket.recv_string().split(" ")
+    meas_id, unique_id = sync_socket.recv_string().split(" ")
 
-#     print(meas_id)
+    print(meas_id)
 
-#     sync_socket.close()
+    sync_socket.close()
 
-#     return meas_id, unique_id
+    return meas_id, unique_id
 
 
 
@@ -71,7 +71,7 @@ plt = TechtilePlotter(realtime=True)
 try:
     while True:
 
-        # meas_id, unique_id = wait_till_go_from_server()
+        meas_id, unique_id = wait_till_go_from_server()
         sleep(0.2)  # wake-up 10 seconds before rover starts to move
 
         positions = []
@@ -85,12 +85,12 @@ try:
         if pos is not None:
             plt.measurements_rt(pos.x, pos.y, pos.z, power_dBm)
         # counter+= 1
-        # meas_name = f"gausbf-ceiling-grid-{meas_id}-{unique_id}-{counter}"
-        # np.save(arr=positions, file=f"../data/positions-{meas_name}")
-        # np.save(arr=values, file=f"../data/values-{meas_name}")
+        meas_name = f"mrt-ceiling-grid-{meas_id}-{unique_id}-{counter}"
+        np.save(arr=positions, file=f"../data/positions-{meas_name}")
+        np.save(arr=values, file=f"../data/values-{meas_name}")
 finally:
     print("Ctrl+C pressed. Exiting loop and saving...")
-    # meas_name = f"gausbf-ceiling-grid-{meas_id}-{unique_id}-{counter}"
-    # np.save(arr=positions, file=f"../data/positions-{meas_name}")
-    # np.save(arr=values, file=f"../data/values-{meas_name}")
+    meas_name = f"mrt-ceiling-grid-{meas_id}-{unique_id}-{counter}"
+    np.save(arr=positions, file=f"../data/positions-{meas_name}")
+    np.save(arr=values, file=f"../data/values-{meas_name}")
     positioner.stop()
